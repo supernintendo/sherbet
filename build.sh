6b163d67aaf687b3d153aa8c0279e6b04a65cf8b
Michael Matyi <arrmatyi@gmail.com>
2014-09-21 21:13:29 -0700

    example-and-js-fix > Fix example and update JavaScript to properly reload CSS.

--- build.sh
+++ build.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-function cleanup {
-    rm build/bundle.js
-    rm bindata.go
-    rmdir build
-}
-cleanup
 mkdir -p build
 cd js && npm install && npm run build
 cd ..
 go get
-go-bindata build
-go build
-cleanup
+go-bindata build && go build
