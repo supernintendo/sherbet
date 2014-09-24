431f79faa95e2a395e03d8660de326bb612d8f82
Michael Matyi <arrmatyi@gmail.com>
2014-09-23 23:45:01 -0700

    watcher-refactor > Refactor watchers to allow multiple watchers and introduce some general improvements.

--- watchers.go
+++ watchers.go
@@ -8,35 +8,55 @@ import (
 	"strings"
 )
 
-func newWatcher(path string, css string) {
-	watcher, err := fsnotify.NewWatcher()
+type Watcher struct {
+	Category     string
+	Create       bool
+	DeliverFile  bool
+	File         string
+	Message      string
+	OnCreate     bool
+	OnDelete     bool
+	OnModify     bool
+	OnRename     bool
+}
+
+func activateWatcher(w Watcher) {
+	activated, err := fsnotify.NewWatcher()
+
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	go func() {
 		for {
 			select {
-			case event := <-watcher.Event:
-				if event.IsModify() {
-					fileSlugs := strings.Split(event.Name, "/")
+			case event := <-activated.Event:
+				slugs := strings.Split(event.Name, "/")
 
-					// Check that the event is related to the reloadCSS file.
-					if fileSlugs[len(fileSlugs)-1] == css {
-						file, err := ioutil.ReadFile(event.Name)
-						if err != nil {
-							fmt.Print("Error reading file.", err)
-						}
-						sendAll("reloadCSS", file)
+				if slugs[len(slugs)-1] == w.File {
+
+					c := event.IsCreate() && w.OnCreate
+					d := event.IsDelete() && w.OnDelete
+					m := event.IsModify() && w.OnModify
+					r := event.IsRename() && w.OnRename
+
+					file, err := ioutil.ReadFile(event.Name)
+
+					if err != nil {
+						fmt.Print("Error reading file.", err)
+					}
+					switch {
+					case c: sendAll(w.Category, make([]byte, 0, 1), w.Message)
+					case d: sendAll(w.Category, make([]byte, 0, 1), w.Message)
+					case m: sendAll(w.Category, file, w.Message)
+					case r: sendAll(w.Category, file, w.Message)
 					}
 				}
-			case err := <-watcher.Error:
+			case err := <-activated.Error:
 				log.Println("error:", err)
 			}
 		}
 	}()
-
-	err = watcher.Watch(path)
+	err = activated.Watch(sherbetFile.RootPath)
 	if err != nil {
 		log.Fatal(err)
 	}
