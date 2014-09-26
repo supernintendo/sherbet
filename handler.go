66d8e3df46443f5bc40021b4c219f49fd76a3de0
Michael Matyi <arrmatyi@gmail.com>
2014-09-25 22:25:23 -0700

    send-on-load > Activate sending watcher payloads on page load and fix discrepancy between server port and JS port.

--- handler.go
+++ handler.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"github.com/gorilla/websocket"
 )
 
 func dealWith(msg []byte, c *websocket.Conn) {
 	switch string(msg) {
 	case "Connected":
-		sendTo(c, "Status", make([]byte, 0, 1), "Connected.")
+		sendWatcherPayload(c)
+	}
+}
+
+func sendWatcherPayload(c *websocket.Conn) {
+	for _, w := range app.Watchers {
+		if w.OnLoad {
+			file, err := ioutil.ReadFile(app.RootPath + "/" + w.File)
+
+			if err != nil {
+				fmt.Print("Error reading file.", err)
+			}
+			sendTo(c, w.Category, file, w.Message)
+		}
 	}
 }
